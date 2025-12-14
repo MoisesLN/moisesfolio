0ad394e01f24e7c13224efe3de7c55330ad18dca
Moises Lopes <moisesbruh5@gmail.com>
2025-12-14 16:32:48 -0300

    feat: header fadeIn animation

--- project/src/components/Header.vue
+++ project/src/components/Header.vue
@@ -29,12 +29,40 @@ export default {
     a {
         color: var(--subtext);
         text-decoration: none;
+        opacity: 0;
         &:hover {
             color: var(--text);
         }
     }
 
+    a:nth-child(1) {
+        animation: fadeIn 500ms ease-out 1 forwards;
+    }
+
+    a:nth-child(2) {
+        animation: fadeIn 500ms ease-out 100ms 1 forwards;
+    }
+
+    a:nth-child(3) {
+        animation: fadeIn 500ms ease-out 200ms 1 forwards;
+    }
+
+    a:nth-child(4) {
+        animation: fadeIn 500ms ease-out 300ms 1 forwards;
+    }
+
     .me {
         margin-right: auto;
+    }
+
+    @keyframes fadeIn {
+        from {
+            opacity: 0;
+            transform: translateY(20px);
+        }
+        to {
+            opacity: 1;
+            transform: translateY(0px);
+        }
     }
 </style>
