892261bb38c9b096def8d26d416c2a9862255d7d
Moises Lopes <moisesbruh5@gmail.com>
2025-12-14 21:07:53 -0300

    fix: correct image paths for deployment

--- project/src/assets/projects.js
+++ project/src/assets/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         title: 'VITRINEPP',
         description: 'Objective: Create an virtual enviroment to collaborate with small business owners in fashion, focusing on giving them more reach on their products.',
-        image: '/vitrinepp.png',
+        image: './vitrinepp.png',
         stack: [
             'VueJS',
             'Django'
@@ -11,7 +11,7 @@ const projects = [
     {
         title: 'IFRN CONECTA',
         description: 'Objective: Build a web app that works as a social media for IFRN students, aiming to strengthen the bonds between students and members of the academic community.',
-        image: '/ifrn-conecta.png',
+        image: './ifrn-conecta.png',
         stack: [
             'VueJS',
             'Django'
@@ -20,7 +20,7 @@ const projects = [
     {
         title: 'CLOCK OF CLOCKS',
         description: 'It’s a clock.. made out of clocks!',
-        image: '/clock-of-clocks.png',
+        image: './clock-of-clocks.png',
         stack: [
             'JS',
             'HTML',
