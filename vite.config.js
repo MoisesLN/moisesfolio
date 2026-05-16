2a51d234c8acf2c8b462dcb8a0314590b51d46e3
Moises Lopes <moisesbruh5@gmail.com>
2026-05-15 21:53:52 -0300

    refactor: build now goes to root folder

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/moisesfolio/',
   plugins: [vue()],
-  root: './project'
+  root: './project',
+  build: {
+    outDir: '../dist/'
+  }
 })
